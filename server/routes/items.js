394392ffa10c5f80f3e1798c0236bc451fdcfaef
Jasmine Daniels <create.jasminedaniels@gmail.com>
2024-04-08 15:31:52 -0400

    update response message in item routes

--- server/routes/items.js
+++ server/routes/items.js
@@ -8,6 +8,7 @@ router.get('/', async (req, res, next) => {
         const items = await Item.findAll();
         res.status(200).json(items);
     } catch (error) {
+        res.status(500).json({ message: `Server Error`});
         next(error);
     }
 })
@@ -19,6 +20,7 @@ router.get('/:id', async (req, res, next) => {
         !item ? res.status(404).json({ message: `Item Not Found` }) :
             res.status(200).json(item);
     } catch (error) {
+        res.status(500).json({ message: `Server Error`});
         next(error);
     }
 })
