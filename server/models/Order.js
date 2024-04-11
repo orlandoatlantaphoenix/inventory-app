ba7e3876de11f4dec4e70cc37e9748adf0bf517e
Jasmine Daniels <create.jasminedaniels@gmail.com>
2024-04-11 12:53:36 -0400

    updated user and order models

--- server/models/Order.js
+++ server/models/Order.js
@@ -3,6 +3,10 @@ const {sequelize} = require('../db')
 
 const Order = sequelize.define("Order", {
   user: Sequelize.STRING,
+  total: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0
+  }
 });
 
 module.exports = {
--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,11 @@ const {sequelize} = require('../db')
 const User = sequelize.define("User", {
   name: Sequelize.STRING,
   email: Sequelize.STRING,
-  password: Sequelize.STRING
+  password: Sequelize.STRING,
+  isAdmin: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0
+  }
 });
 
 module.exports = {
